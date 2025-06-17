d843db6cb3555027071c8651b2a1ce7634db58e0
Orson Teodoro <orsonteodoro@hotmail.com>
2025-06-17 12:09:10 -0700

    Add comment about chacha20 vs /dev/random

--- src/ycmd.c
+++ src/ycmd.c
@@ -2677,6 +2677,11 @@ void generate_entropy(uint8_t *key) {
 	}
 }
 
+/* Trade-off table for CSPRNG
+ *                Entropy | Security | Performance | Estimated worst case startup time
+ * Chacha20     | 8/10    | 9/10     | 9/10        | 1 microsecond or less
+ * /dev/random  | 9/10    | 9/10     | 6/10        | A few seconds
+ */
 void csprng_chacha20_get_key(uint8_t *key, size_t key_size) {
 #if defined(USE_NETTLE)
 	uint8_t chacha_key[CSPRNG_CHACHA20_KEY_SIZE];
